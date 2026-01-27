dc6e57ce23e24ceb45e67ee87972d31cd00b9b4d
Alex Prestes <alex.prestes@outlook.com.br>
2026-01-27 15:21:36 -0300

    bindkey corretas

--- .config/zsh/keybinds.zsh
+++ .config/zsh/keybinds.zsh
@@ -8,22 +8,17 @@ autoload -Uz zkbd
 # Backspace (muito comum precisar fixar)
 bindkey '^?' backward-delete-char
 
-# Home / End / Delete via terminfo
-bindkey '^[[H' beginning-of-line
-[[ -n ${terminfo[kend]}  ]] || bindkey "${terminfo[kend]}"  end-of-line
-[[ -n ${terminfo[kdch1]} ]] || bindkey "${terminfo[kdch1]}" delete-char
+# --- Atalhos de Teclado (Bindkeys) ---
+# Home e End
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
 
-# PageUp / PageDown (fallback para sequências comuns + terminfo se existir)
-[[ -n ${terminfo[kpp]}  ]] || bindkey "${terminfo[kpp]}"  up-line-or-history
-[[ -n ${terminfo[knp]}  ]] || bindkey "${terminfo[knp]}"  down-line-or-history
-bindkey '^[[5~' up-line-or-history
-bindkey '^[[6~' down-line-or-history
+# Del (Delete)
+bindkey "^[[3~" delete-char
 
-# Setas: prefira terminfo, mas mantém fallback do seu arquivo
-[[ -n ${terminfo[kcuu1]} ]] || bindkey "${terminfo[kcuu1]}" up-line-or-search
-[[ -n ${terminfo[kcud1]} ]] || bindkey "${terminfo[kcud1]}" down-line-or-search
-[[ -n ${terminfo[kcub1]} ]] || bindkey "${terminfo[kcub1]}" backward-char
-[[ -n ${terminfo[kcuf1]} ]] || bindkey "${terminfo[kcuf1]}" forward-char
+# Page Up e Page Down (Navegar no histórico)
+bindkey "^[[5~" up-line-or-history
+bindkey "^[[6~" down-line-or-history
 
 bindkey '^[[A' up-line-or-search
 bindkey '^[[B' down-line-or-search

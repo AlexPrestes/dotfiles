7444a943b92feb6163e7a781e07302eee9a04bb9
Alex Prestes <alex.prestes@outlook.com.br>
2026-01-27 15:12:38 -0300

    keybind

--- .config/zsh/keybinds.zsh
+++ .config/zsh/keybinds.zsh
@@ -9,21 +9,21 @@ autoload -Uz zkbd
 bindkey '^?' backward-delete-char
 
 # Home / End / Delete via terminfo
-[[ -n ${terminfo[khome]} ]] && bindkey "${terminfo[khome]}" beginning-of-line
-[[ -n ${terminfo[kend]}  ]] && bindkey "${terminfo[kend]}"  end-of-line
-[[ -n ${terminfo[kdch1]} ]] && bindkey "${terminfo[kdch1]}" delete-char
+[[ -n ${terminfo[khome]} ]] || bindkey "${terminfo[khome]}" beginning-of-line
+[[ -n ${terminfo[kend]}  ]] || bindkey "${terminfo[kend]}"  end-of-line
+[[ -n ${terminfo[kdch1]} ]] || bindkey "${terminfo[kdch1]}" delete-char
 
 # PageUp / PageDown (fallback para sequências comuns + terminfo se existir)
-[[ -n ${terminfo[kpp]}  ]] && bindkey "${terminfo[kpp]}"  up-line-or-history
-[[ -n ${terminfo[knp]}  ]] && bindkey "${terminfo[knp]}"  down-line-or-history
+[[ -n ${terminfo[kpp]}  ]] || bindkey "${terminfo[kpp]}"  up-line-or-history
+[[ -n ${terminfo[knp]}  ]] || bindkey "${terminfo[knp]}"  down-line-or-history
 bindkey '^[[5~' up-line-or-history
 bindkey '^[[6~' down-line-or-history
 
 # Setas: prefira terminfo, mas mantém fallback do seu arquivo
-[[ -n ${terminfo[kcuu1]} ]] && bindkey "${terminfo[kcuu1]}" up-line-or-search
-[[ -n ${terminfo[kcud1]} ]] && bindkey "${terminfo[kcud1]}" down-line-or-search
-[[ -n ${terminfo[kcub1]} ]] && bindkey "${terminfo[kcub1]}" backward-char
-[[ -n ${terminfo[kcuf1]} ]] && bindkey "${terminfo[kcuf1]}" forward-char
+[[ -n ${terminfo[kcuu1]} ]] || bindkey "${terminfo[kcuu1]}" up-line-or-search
+[[ -n ${terminfo[kcud1]} ]] || bindkey "${terminfo[kcud1]}" down-line-or-search
+[[ -n ${terminfo[kcub1]} ]] || bindkey "${terminfo[kcub1]}" backward-char
+[[ -n ${terminfo[kcuf1]} ]] || bindkey "${terminfo[kcuf1]}" forward-char
 
 bindkey '^[[A' up-line-or-search
 bindkey '^[[B' down-line-or-search
